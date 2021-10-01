580c32e8431825edbb389b623ad247f79ca5d653
abdullahth <abdullahthurwatx@gmail.com>
2021-10-01 08:58:48 +0200

    Custom Home Page Search TextFormField

---
+++ Development/client/lib/views/home-page/components/search_bar/search_bar.dart
@@ -0,0 +1,52 @@
+import 'package:client/preferences/enums/icons.dart';
+import 'package:dims_config/dims_config.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePageSearchBar extends StatelessWidget {
+  const HomePageSearchBar({
+    Key? key,
+    required this.searchCallback,
+  }) : super(key: key);
+
+  final Function(String searchToken) searchCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    final dims = DevicesDimsConfig(context);
+    return ResponsivePadding(
+      child: Container(
+          height: dims.deviceHeight * 0.06,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(dims.configWidth(10)),
+          ),
+          child: TextFormField(
+            decoration: InputDecoration(
+                hintText: 'search'.tr,
+                hintStyle: TextStyle(color: Colors.grey.shade700),
+                border: InputBorder.none,
+                prefixIcon: ResponsivePadding(
+                  child: Container(
+                    width: dims.deviceWidth * 0.05,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              AppIcons.homePageSearchIcon.iconAssetName)),
+                    ),
+                  ),
+                  padding: SymetricPadding(
+                    context: context,
+                    horizontal: 2,
+                    vertical: 1,
+                  ),
+                )),
+          )),
+      padding: SymetricPadding(
+        context: context,
+        horizontal: 8,
+        vertical: 12,
+      ),
+    );
+  }
+}
